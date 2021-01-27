3eaf4c02938fb233b343c328028b3c9667cd252a
Aidan Holloway <aidan.holloway@younglings.africa>
2021-01-27 13:55:38 +0200

    Uploaded latest version
    Added more dialogue content, corrected settings to store save game files in the correct location.

---
+++ Version 0.61/Program Code/Testing1/data/175E453F85D7814E9849A1B3F4E8EF8DE245.json.js
@@ -0,0 +1,40 @@
+GS.dataCache['175E453F85D7814E9849A1B3F4E8EF8DE245'] = {
+    "uid": "175E453F85D7814E9849A1B3F4E8EF8DE245",
+    "isLoaded": true,
+    "lastModificationTime": 0,
+    "items": {
+        "name": "Tendou, Yui",
+        "type": "data_record",
+        "order": 5,
+        "category": "characters",
+        "id": "175E453F85D7814E9849A1B3F4E8EF8DE245",
+        "isFolder": false,
+        "parentId": "ROOT",
+        "data": {
+            "name": {
+                "lcId": null,
+                "defaultText": "Tendou, Yui"
+            },
+            "textColor": {
+                "red": 255,
+                "green": 255,
+                "blue": 255,
+                "alpha": 255
+            },
+            "defaultExpressionId": "BF7CB12110894142C299D341A545C910AA86",
+            "timeTalkingToVoiceVolume": true,
+            "talkingVolume": 5,
+            "lipSyncSensitivity": 1,
+            "breathIntensity": 1,
+            "idleIntensity": 1,
+            "params": [],
+            "index": "175E453F85D7814E9849A1B3F4E8EF8DE245"
+        }
+    },
+    "summary": [
+        "name",
+        "type",
+        "order"
+    ],
+    "externalItems": []
+}
---
+++ Version 0.61/Program Code/Testing1/data/DD3E7A1315732343B37AFD6676343BD7B25C.json.js
@@ -0,0 +1,40 @@
+GS.dataCache['DD3E7A1315732343B37AFD6676343BD7B25C'] = {
+    "uid": "DD3E7A1315732343B37AFD6676343BD7B25C",
+    "isLoaded": true,
+    "lastModificationTime": 0,
+    "items": {
+        "name": "Himegami, Azuki",
+        "type": "data_record",
+        "order": 7,
+        "category": "characters",
+        "id": "DD3E7A1315732343B37AFD6676343BD7B25C",
+        "isFolder": false,
+        "parentId": "ROOT",
+        "data": {
+            "name": {
+                "lcId": null,
+                "defaultText": "Himegami, Azuki"
+            },
+            "textColor": {
+                "red": 255,
+                "green": 255,
+                "blue": 255,
+                "alpha": 255
+            },
+            "defaultExpressionId": "BF7CB12110894142C299D341A545C910AA86",
+            "timeTalkingToVoiceVolume": true,
+            "talkingVolume": 5,
+            "lipSyncSensitivity": 1,
+            "breathIntensity": 1,
+            "idleIntensity": 1,
+            "params": [],
+            "index": "DD3E7A1315732343B37AFD6676343BD7B25C"
+        }
+    },
+    "summary": [
+        "name",
+        "type",
+        "order"
+    ],
+    "externalItems": []
+}
